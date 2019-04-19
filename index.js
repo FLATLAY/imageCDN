aab0ab1fcc5b3519b64344fbeaeeb018f69d6c0d
MaryamMahmoodi <mrm.mahmoodi@gmail.com>
2019-04-19 22:14:58 +0430

    s3 on test

--- index.js
+++ index.js
@@ -23,16 +23,12 @@ var path = require("path");
 var fs = require('fs');
 const sharp = require('sharp');
 //~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
-//const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
+/*const aws = require("aws-sdk");
 aws.config.update({
-  secretAccessKey: process.env.SECRETACCESSKEY,
-  accessKeyId: process.env.ACCESSKEYID,
   region: process.env.REGION // region of your bucket
-});
-
-const s3 = new aws.S3();
+});*/
 //~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
+import S3 from 'aws-s3';
 
 app.post("/upload", upload.single("image"), function(req, res) {
 	var path =res.req.file.path;
@@ -50,35 +46,75 @@ app.post("/upload", upload.single("image"), function(req, res) {
 	//console.log(checksum);
 	var response={}
 	var destination= res.req.file.destination;
+	var imageNameOriginag= destination + "/" + checksum ;
 	var imageNameSmall= destination + "/" + checksum + "_small.jpg";
 	var imageNameLarge= destination + "/" + checksum + "_large.jpg";
 	return new Promise(function(resolve, reject) {
-	sharp(path)
-    .resize(200,200)
-    .toBuffer()
-    .then( data => {
-        fs.writeFileSync(imageNameSmall, data);
-		response.message = "post Successful";
-        res.json(response);
-        resolve(response);
-    })
-    .catch( err => {
-        console.log(err);
-        reject (err);
-    });
-    // 1800 * 1800
-	sharp(path)
-    .resize(1800,1800)
-    .toBuffer()
-    .then( data => {
-        fs.writeFileSync(imageNameLarge, data);
-		response.message = "post Successful";
-        res.json(response);
-        resolve(response);
-    })
-    .catch( err => {
-        console.log(err);
-        reject (err);
-    });
+		for (var i = 0; i < 3; i++) {
+			if (imageNameSmall) {
+			sharp(path)
+    		.resize(200,200)
+    		.toBuffer()
+    		.then( data => {
+        	var imageSmall = fs.writeFileSync(imageNameSmall, data);
+			const config = {
+	    		bucketName: 'upload-file-flatlay',
+			    dirName: imageSmall, 
+			    region: process.env.REGION ,
+			    accessKeyId:  process.env.ACCESSKEYID,
+			    secretAccessKey: process.env.SECRETACCESSKEY
+			}
+			response.message = "post Successful";
+        	res.json(response);
+        	resolve(response);
+    	})
+    	.catch( err => {
+        	console.log(err);
+        	reject (err);
+    	});
+
+		} if(imageNameLarge){
+			sharp(path)
+		    .resize(1800,1800)
+		    .toBuffer()
+		    .then( data => {
+		        var imageLarge = fs.writeFileSync(imageNameLarge, data);
+        	const config = {
+	    		bucketName: 'upload-file-flatlay',
+			    dirName: imageLarge, 
+			    region: process.env.REGION ,
+			    accessKeyId:  process.env.ACCESSKEYID,
+			    secretAccessKey: process.env.SECRETACCESSKEY
+			}
+				response.message = "post Successful";
+		        res.json(response);
+		        resolve(response);
+		    })
+		    .catch( err => {
+		        console.log(err);
+		        reject (err);
+		    });
+			} else {
+			sharp(path)
+		    .toBuffer()
+		    .then( data => {
+		        var imageOriginag= fs.writeFileSync(imageNameOriginag, data);
+        	const config = {
+	    		bucketName: 'upload-file-flatlay',
+			    dirName: imageOriginag, 
+			    region: process.env.REGION ,
+			    accessKeyId:  process.env.ACCESSKEYID,
+			    secretAccessKey: process.env.SECRETACCESSKEY
+			}
+				response.message = "post Successful";
+		        res.json(response);
+		        resolve(response);
+		    })
+		    .catch( err => {
+		        console.log(err);
+		        reject (err);
+		    });	
+			}
+		} //end of for
 	});//Promise
 });
