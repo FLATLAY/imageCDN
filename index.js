8b1b3f976c040677ec05d1a7a6cc9403edab3800
MaryamMahmoodi <mrm.mahmoodi@gmail.com>
2019-04-19 19:57:09 +0430

    it's Ok and file upload with 3 Size ,bot in console I have this comment-> Cannot set headers after they are sent to the client

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const s3 = new aws.S3();
 //~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
 
 app.post("/upload", upload.single("image"), function(req, res) {
-	//console.log(res.req.file.filename);
 	var path =res.req.file.path;
 	function base64_encode(file) {
 
@@ -44,7 +43,6 @@ app.post("/upload", upload.single("image"), function(req, res) {
       return new Buffer.from(bitmap).toString('base64');
     }
    	var encodedFile= base64_encode(path);
-   	//console.log(encodedFile);
    	var checksum = crypto
       .createHash("SHA256")
       .update(encodedFile)
@@ -52,24 +50,35 @@ app.post("/upload", upload.single("image"), function(req, res) {
 	//console.log(checksum);
 	var response={}
 	var destination= res.req.file.destination;
-	var imageName= destination + "/" + checksum + "_small.jpg";
-	console.log(imageName);
+	var imageNameSmall= destination + "/" + checksum + "_small.jpg";
+	var imageNameLarge= destination + "/" + checksum + "_large.jpg";
 	return new Promise(function(resolve, reject) {
-	sharp(imageName)
+	sharp(path)
     .resize(200,200)
     .toBuffer()
     .then( data => {
-    	//{ imageName, encoding: null }
-        fs.writeFileSync(imageName, data);
+        fs.writeFileSync(imageNameSmall, data);
 		response.message = "post Successful";
         res.json(response);
         resolve(response);
-		//console.log('post DONE');
     })
     .catch( err => {
         console.log(err);
         reject (err);
     });
-	
+    // 1800 * 1800
+	sharp(path)
+    .resize(1800,1800)
+    .toBuffer()
+    .then( data => {
+        fs.writeFileSync(imageNameLarge, data);
+		response.message = "post Successful";
+        res.json(response);
+        resolve(response);
+    })
+    .catch( err => {
+        console.log(err);
+        reject (err);
+    });
 	});//Promise
 });
